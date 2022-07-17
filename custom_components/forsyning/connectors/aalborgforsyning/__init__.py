1debef34c937f90b46e2cdb93bf3fd5cc1e35f6b
Malene <malene@trab.dk>
2022-07-17 10:19:53 +0200

    Add API link

---
+++ custom_components/forsyning/connectors/aalborgforsyning/__init__.py
@@ -0,0 +1,4 @@
+"""Integration til Aalborg forsyning.
+
+API dokumentation: https://aalborgforsyning.dk/erhverv/energioptimering/datadeling-for-fjernaflaeste-varmemalere/api-for-adgang-til-malerdata
+"""
